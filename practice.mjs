60e35722195db92224469699d447757a3d7f5518
Taeil Kwak <taeil2@gmail.com>
2020-04-21 14:36:24 -0500

    completed the second drill

--- practice.mjs
+++ practice.mjs
@@ -9,7 +9,22 @@
              return result;
            }
    }
-  
+
+  console.log(loaf.flour);
+  console.log(loaf.water);
+
   let finalResult = loaf.hydration();
   console.log(finalResult);
-  
+
+  // 2. Iterating over an object's properties
+  let foobarObj = {
+    foo: 390,
+    bar: "hello",
+    fum: "fo fo",
+    quux: 839,
+    spam: 99999999
+  }
+
+  for (let key in foobarObj) {
+    console.log(key, foobarObj[key]);
+  }
